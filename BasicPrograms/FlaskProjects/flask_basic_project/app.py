2afa09ff3f4278984e1e9f0fe59bebbd7851c2bf
Saurabh Kahar <kaharsaurabh2017@gmail.com>
2024-10-03 14:24:21 +0530

    Python program - Learnings

--- BasicPrograms/FlaskProjects/flask_basic_project/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html', username="Saurabh")
-
-app.run(debug=True)
